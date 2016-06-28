60f6a2d6f7236ad6f4bb4c01ecf1fd2c71e0d531
Josh Sullivan <josh@misllc.com>
2016-06-28 12:59:55 -0400

    updated admin

--- webhook/admin.py
+++ webhook/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from .models import NewLead
 
 class NewLeadAdmin(admin.ModelAdmin):
-    fields = ['first_name', 'last_name', 'email', 'telephone']
+    fields = ['first_name', 'last_name', 'email', 'telephone', 'form_id']
     
 admin.site.register(NewLead, NewLeadAdmin)
