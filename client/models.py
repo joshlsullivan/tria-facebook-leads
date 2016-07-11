41b417b45d292a4a39e5759a713f6b2b0c95237b
Josh Sullivan <josh@misllc.com>
2016-07-10 23:31:15 -0500

    updated db

--- client/models.py
+++ client/models.py
@@ -6,9 +6,9 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Client(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    cell_phone = models.CharField(max_length=(12), default="")
-    company = models.CharField(max_length=120, default="")
-    form_id = models.CharField(max_length=120, default="")
+    cell_phone = models.CharField(max_length=(12), blank=True)
+    company = models.CharField(max_length=120, blank=True)
+    form_id = models.CharField(max_length=120, blank=True)
     
     def __str__(self):
         return self.cell_phone
