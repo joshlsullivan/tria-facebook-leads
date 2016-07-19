58777095978cfd00c80c814989d4b506b0a0effe
Josh Sullivan <josh@misllc.com>
2016-07-19 22:51:30 +0000

    updated views

--- webhook/views.py
+++ webhook/views.py
@@ -50,13 +50,6 @@ def subscribe_mailchimp(mailchimp_dc, mailchimp_list, mailchimp_api, first_name,
         }
     )
 
-def get_values(data, name):
-    for data_element in data.get('field_data'):
-        if data_element.get('name') == name:
-            return data_element.get('values')
-
-    return None
-
 class WebhookView(generic.View):
     #Verifies the toke with Facebook app
     def get(self, request, *args, **kwargs):
@@ -85,17 +78,26 @@ class WebhookView(generic.View):
         form_id = str(data['form_id'])
         ad_id = str(data['ad_id'])
         clients = User.objects.all()
-        first_name = get_values(data, 'first_name')[0]
-        last_name = get_values(data, 'last_name')[0]
-        email = get_values(data, 'email')[0]
-        telephone = get_values(data, 'phone_number')[0]
+        first_name = get_values('first_name')[0]
+        last_name = get_values('last_name')[0]
+        email = get_values('email')[0]
+        telephone = get_values('phone_number')[0]
         for client in clients:
             if data:
-                client_email = client.email
-                client_first_name = client.first_name
-                client_last_name = client.last_name
-                e = Leads(first_name=first_name, last_name=last_name, email=email, telephone=telephone, form_id=form_id, leadgen_id=leadgen_id, ad_id=ad_id)
-                e.save()
-                if client.client.facebook_form_id == form_id:
-                    send_tagged_message(client_email=client_email, first_name=first_name, last_name=last_name, email=email, telephone=telephone, client_first_name=client_first_name, client_last_name=client_last_name)
+                for entry in data['field_data']:
+                    if entry['name'] == 'first_name':
+                        first_name = entry['values'][0]
+                    elif entry['name'] == 'last_name':
+                        last_name = entry['values'][0]
+                    elif entry['name'] == 'email':
+                        email = entry['values'][0]
+                    elif entry['name'] == 'phone_number':
+                        telephone = entry['values'][0]
+                    client_email = client.email
+                    client_first_name = client.first_name
+                    client_last_name = client.last_name
+                    e = Leads(first_name=first_name, last_name=last_name, email=email, telephone=telephone, form_id=form_id, leadgen_id=leadgen_id, ad_id=ad_id)
+                    e.save()
+                    if client.client.facebook_form_id == form_id:
+                        send_tagged_message(client_email=client_email, first_name=first_name, last_name=last_name, email=email, telephone=telephone, client_first_name=client_first_name, client_last_name=client_last_name)
         return HttpResponse()
