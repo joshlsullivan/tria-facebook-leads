7558d21d5872b83b2096444460ed55597992956a
Josh Sullivan <josh@misllc.com>
2016-07-21 10:27:39 -0500

    removed code

--- leads/admin.py
+++ leads/admin.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.contrib import admin
 from .models import Leads
+from datetime import date
+
+
 
 class NewLeadAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'telephone', 'get_client')
