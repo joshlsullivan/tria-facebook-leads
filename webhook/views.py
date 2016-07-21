971b530aa09b210d91d023e48ae90a7219f63338
Josh Sullivan <josh@misllc.com>
2016-07-21 14:11:49 -0500

    updated

--- webhook/views.py
+++ webhook/views.py
@@ -35,23 +35,20 @@ def send_tagged_message(client_email, first_name, last_name, email, telephone, c
         }
     )
 
-def subscribe_mailchimp(c, client_mailchimp_dc, client_mailchimp_list, client_mailchimp_api, first_name, last_name, email):
+def subscribe_mailchimp(client_mailchimp_dc, client_mailchimp_list, client_mailchimp_api, first_name, last_name, email):
     #mailchim_dc referes to the mailchimp datacenter in api e.g. us5
-    if c.client.has_mailchimp:
-        return requests.post(
-            "https://" + client_mailchimp_dc + ".api.mailchimp.com/3.0/lists/" + client_mailchimp_list + "/members/",
-            auth=('api', client_mailchimp_api),
-            data={
-                "email_address": email,
-                "status": "pending",
-                "merge_fields": {
-                "FNAME": first_name,
-                "LNAME": last_name
-                }
+    return requests.post(
+        "https://" + client_mailchimp_dc + ".api.mailchimp.com/3.0/lists/" + client_mailchimp_list + "/members/",
+        auth=('api', client_mailchimp_api),
+        data={
+            "email_address": email,
+            "status": "pending",
+            "merge_fields": {
+            "FNAME": first_name,
+            "LNAME": last_name
             }
-        )
-    else:
-        print("No subscription sent.")
+        }
+    )
 
 def get_values(data, name):
     for data_element in data.get('field_data'):
@@ -103,15 +100,6 @@ class WebhookView(View):
                 e = Leads(first_name=first_name, last_name=last_name, email=email, telephone=telephone, form_id=form_id, leadgen_id=leadgen_id, ad_id=ad_id)
                 e.save()
                 if c.client.facebook_form_id == form_id:
-                    subscribe_mailchimp(
-                        c,
-                        client_mailchimp_dc=client_mailchimp_dc,
-                        client_mailchimp_list=client_mailchimp_list,
-                        client_mailchimp_api=client_mailchimp_api,
-                        first_name=first_name,
-                        last_name=last_name,
-                        email=email
-                        )
                     send_tagged_message(
                         client_email=client_email,
                         first_name=first_name,
